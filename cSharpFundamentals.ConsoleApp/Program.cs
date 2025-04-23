af83fcdccb32854e43e81c722e6242c84497447b
sayed_mamdouh <eldeebsayed22@gmail.com>
2025-04-23 23:43:37 +0200

    Add project files.

---
+++ cSharpFundamentals.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace cSharpFundamentals.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many large mat ..?  (if you dont have type -> 0)");
+
+            int? large =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many small mat ..? (if you dont have type -> 0)");
+
+            int? small = Convert.ToInt32(Console.ReadLine());
+
+            double? result = ((large * 35) + (small * 25)) - .06;
+
+            Console.WriteLine($"=====================================\n your Amount  is  ${result}");
+
+        }
+
+    }
+}
