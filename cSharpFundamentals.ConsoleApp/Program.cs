ce132a5f1bc9cfc331d64fea6030be93d23afad8
sayed_mamdouh <eldeebsayed22@gmail.com>
2025-04-24 14:00:55 +0200

    change the q text

--- cSharpFundamentals.ConsoleApp/Program.cs
+++ cSharpFundamentals.ConsoleApp/Program.cs
@@ -5,16 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to islam carpet clean service ..!\n");
-            
-            
 
-            Console.WriteLine("Enter Number of small carpet ..?  (if you dont have small carpet type number 0)");
+
+
+            Console.WriteLine("Enter Number of small carpet ..? ");
 
             int? small = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Number of large carpet ..? (if you dont have large carpet type number 0)");
+            Console.WriteLine("Enter Number of large carpet ..? ");
 
-            int? large =Convert.ToInt32(Console.ReadLine());
+            int? large = Convert.ToInt32(Console.ReadLine());
 
             double? result = large * 35 + small * 25 + 6.6;
 
@@ -26,8 +26,11 @@
 
             Console.WriteLine("cost : $" + cost);
             Console.WriteLine("Tax : $" + tax);
-      
+
             Console.WriteLine($"=====================================\n Total estimate : ${result} \n This estimate is valid for 30 days");
+
+
+
 
         }
 
