bbf055717ec2e24759143e1058bb29ad96e8c104
sayed_mamdouh <eldeebsayed22@gmail.com>
2025-04-25 22:35:46 +0300

    variables update

--- cSharpFundamentals.ConsoleApp/Program.cs
+++ cSharpFundamentals.ConsoleApp/Program.cs
@@ -6,30 +6,26 @@
         {
             Console.WriteLine("Estimate for carpet cleaning service ..!\n");
 
-            Console.WriteLine("Enter Number of small carpet ..? ");
+            Console.WriteLine("Enter Number of small carpet ..?");
 
             int? small = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Number of large carpet ..? ");
+            Console.WriteLine("Enter Number of large carpet ..?");
 
             int? large = Convert.ToInt32(Console.ReadLine());
 
-            double? result = large * 35 + small * 25 + 6.6;
+            double tax = 6.6;
+
+            double? result = large * 35 + small * 25 + tax;
 
             int? cost = large * 35 + small * 25;
 
             Console.WriteLine("\n");
 
-            double tax = 6.6;
 
-            Console.WriteLine($"cost : {cost:C}");
-            Console.WriteLine($"Tax : {tax:C}");
-
-            Console.WriteLine($"=====================================\n Total estimate : {result:C} \n This estimate is valid for 30 days");
-
-
-
-
+            Console.WriteLine("cost : $"+cost);
+            Console.WriteLine("Tax : $"+tax);
+            Console.WriteLine($"=====================================\n Total estimate : ${result} \n This estimate is valid for 30 days");
         }
 
     }
